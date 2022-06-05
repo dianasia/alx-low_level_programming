6a5ff272e1c12ed41f46ee084475828db95fb882
dianasia <dianasiasendeka@gmail.com>
2022-06-05 12:57:33 -0700

    first

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,21 +10,21 @@
 int main (void)
 {
 	int n, lastdigits;
-
+	
 	srand(time(0));
 	n = rand() -Rand_Max / 2;
 	if (n>0)
-       {
-           printf("%d is positive\n",n);
+	{
+	       printf("%d is positive\n",n);
        }
-     else if (n==0)
-      {
-           printf("%d is negative\n",);
+	else if (n==0)
+     {
+	      printf("%d is negative\n",);
       }
-      else
+	else
       {
-	   printf("%d is negative\n",);
+	      printf("%d is negative\n",);
       }
-       return (0);
+      return (0);
 }
 
