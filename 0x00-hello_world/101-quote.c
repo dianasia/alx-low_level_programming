7766a26ad622ac89851ca69d88e0217b1abb1fd6
dianasia <dianasiasendeka@gmail.com>
2022-06-05 17:08:56 -0700

    ten

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
- * Return:Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
