3628c97dd04326d1632cce5f42ebdaa45791f862
dianasia <dianasiasendeka@gmail.com>
2022-06-08 11:53:33 -0700

    zero

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+/**
+* _abs - check abs .
+* @x: integer.
+* Return: Always 0.
+*/
+int _abs(int x)
+{
+	if (x > 0)
+	{
+		return (x);
+	}
+	else if (x < 0)
+	{
+		return (x = x * -1);
+	}
+	else
+		return (0);
+}
