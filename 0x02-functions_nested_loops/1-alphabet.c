7fa0ee2abb8a6428f7552d410f66c52693bf8466
dianasia <dianasiasendeka@gmail.com>
2022-06-07 18:05:19 -0700

    two

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *print_alphabet - This is a print_alphabet function
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
