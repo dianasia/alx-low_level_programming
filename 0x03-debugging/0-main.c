de61f84a5f03b7a80358bd996982ddcd408b4022
Diana <105008492+dianasia@users.noreply.github.com>
2022-06-09 14:55:24 -0700

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - function that tests either positive or negative.
+ * main - check the code for Holberton School students.
  *
- * Return: 0
+ * Return: Always 0.
  */
-int main(void) 
+int main(void)
 {
 	int i;
 
