7c6898fafbb6d067d90dc347de3e531ed1e5087e
dianasia <dianasiasendeka@gmail.com>
2022-06-27 02:17:14 -0700

    Mallocs

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,13 +16,13 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	for (n = 0; str[n] != '\0'; n++)
+		;
 		strnew = (char *)malloc(n + 1 * sizeof(char));
 	if (strnew != NULL)
 	{
 		for (i = 0; str[i] != '\0'; i++)
 			strnew[i] = str[i];
-	}
-	else
+	}else
 	{
 		return (NULL);
 	}
